52c18c93883d8845cd89785809c32a976987f1db
Salvador Espinosa <espinosalvador94@outlook.com>
2023-02-09 23:14:24 -0600

    last commit

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generatorMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-console.log("readme gern running");
+// Include packages needed for this application
+
+// Create an array of questions for user input
 const questions = [
   {
     type: "input",
@@ -20,10 +22,10 @@ const questions = [
   },
   {
     type: "list",
-    name: "licence",
+    name: "license",
     message: "What licence does your proyect use?",
     choices: ["MIT", "Apache 2.0", "GPL v3.0", "None"],
-    validate: (LicenseInput) => {
+    validate: (licenseInput) => {
       if (licenseInput) {
         return true;
       } else {
@@ -49,8 +51,8 @@ const questions = [
     type: "input",
     name: "installation",
     message: "What steps are needed to install this project?",
-    validate: (InstallationInput) => {
-      if (InstallationInput) {
+    validate: (installationInput) => {
+      if (installationInput) {
         return true;
       } else {
         console.log(
@@ -73,13 +75,77 @@ const questions = [
       }
     },
   },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please provide a contribution guidelines for this project",
+    validate: (contributingInput) => {
+      if (contributingInput) {
+        return true;
+      } else {
+        console.log("Please, enter a correct usage for this project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide with test instructions for this project",
+    validate: (testInput) => {
+      if (testInput) {
+        return true;
+      } else {
+        console.log("Please, enter test instructions for this project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please provide your Github username",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please, enter your github username");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please, enter your email address");
+        return false;
+      }
+    },
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("the file was saved!");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generatorMarkdown(answers);
+    writeToFile("generatorREADME.md", markdown);
+  });
+}
 
 // Function call to initialize app
 init();
